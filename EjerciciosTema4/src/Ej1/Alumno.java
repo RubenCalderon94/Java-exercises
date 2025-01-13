56457e1a46ab6a53aea0f4fb6fd5ffaf9b6787e9
RubenCalderon94 <rubencalderonfregenal@gmail.com>
2025-01-13 12:55:06 +0100

    Ej1 Terminado

--- EjerciciosTema4/src/Ej1/Alumno.java
+++ EjerciciosTema4/src/Ej1/Alumno.java
@@ -1,4 +1,88 @@
 package Ej1;
 
+/*Diseña un programa en Java que trabaje con una clase llamada Alumno. Serán
+atributos del alumno su número de expediente, nombre, apellidos, fecha de nacimiento
+y curso en el que se matricula. La clase debe incluir un constructor y los métodos get
+y set correspondientes y toString. Instancia varios objetos de esta clase y prueba los
+métodos creados.*/
+
+
+import java.time.LocalDate;
+
 public class Alumno {
+
+    //ATRIBUTOS
+
+    private int expediente;
+    private String nombre;
+    private String apellidos;
+    private LocalDate fechaNacimiento;
+    private String curso;
+
+
+    //Constructores
+    public Alumno(int expediente, String nombre, String apellidos, LocalDate fechaNacimiento, String curso) {
+        this.expediente=expediente;
+        this.nombre=nombre;
+        this.apellidos=apellidos;
+        this.fechaNacimiento=fechaNacimiento;
+        this.curso=curso;
+
+    }
+
+    //GET Y SET
+
+    public int getExpediente() {
+        return expediente;
+    }
+    public void setExpediente(int expediente) {
+        this.expediente = expediente;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellidos() {
+        return apellidos;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    public String getCurso() {
+        return curso;
+    }
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    @Override
+    public String toString() {
+        return"Expediente: "+ this.expediente +
+                " Nombre: "+ this.nombre +
+                " Apellidos: "+ this.apellidos +
+                " Fecha Nacimiento: "+ this.fechaNacimiento +
+                " Curso: "+ this.curso;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- EjerciciosTema4/src/Ej1/Main.java
+++ EjerciciosTema4/src/Ej1/Main.java
@@ -1,4 +1,24 @@
 package Ej1;
 
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+
+        LocalDate f = LocalDate.of(2000, 12, 19);
+        LocalDate f1= LocalDate.of(2011, 03, 15);
+
+        Alumno alumno1 = new Alumno(23, "Juan", "Ruiz", f, "python");
+        Alumno alumno2 = new Alumno(24, "Pepe", "Gomez", f1, "C++");
+
+
+        System.out.println("Alumno 1:"+ alumno1.getNombre()+" "+ alumno1.getApellidos());
+        System.out.println("Alumno 2:"+ alumno2.getNombre()+" "+ alumno2.getApellidos());
+
+
+        System.out.println(alumno1.toString());
+        System.out.println(alumno2.toString());
+
+
+    }
 }
