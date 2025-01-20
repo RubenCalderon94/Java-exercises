e40187fde018d8d7f55e1ceaaa3c1b6667986ba0
RubenCalderon94 <rcgfregenal@gmail.com>
2025-01-20 15:26:01 +0100

    Finished

---
+++ EjerciciosTema4/src/Ej4/DvdCine.java
@@ -0,0 +1,35 @@
+package Ej4;
+
+/*
+Diseña un programa Java que trabaje con una clase llamada DvdCine.
+a. Su atributos serán: título, director, productora, actores principales,
+género, resumen y duración.
+b. Debe incluir un constructor que reciba como parámetros los atributos.
+c. Crea un método llamado esThriller que devuelva true o false según la
+película sea o no de este género.
+d. Crea todos los métodos gets/sets.
+e. Crea un método llamada mismaProductora que reciba un objeto de la
+clase DvdCine como parámetro. Este método devuelve true o false si
+el objeto utilizado con el método es de la misma productora que el
+pasado como parámetro.
+f. Crea objetos y prueba estos métodos.
+
+ */
+
+public class DvdCine {
+
+    //ATRIBUTOS
+
+    private String titulo;
+    private String director;
+    private String productora;
+    private String actores_principales;
+    private String genero;
+    private String resumen;
+    private double duracion;
+
+
+
+
+
+}
